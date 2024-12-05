6d51be3f8a64b405ea2b08438c2e00825be77256
Peaches_MLG <PeachesMLG100@gmail.com>
2024-12-05 23:10:28 +0000

    Testing

--- src/handlers/cardSpawnHandler.ts
+++ src/handlers/cardSpawnHandler.ts
@@ -7,54 +7,63 @@ export const cardSpawnHandler = async (
   client: Client,
 ) => {
   await saveCardSpawn(cardSpawn);
-  setTimeout(async () => await sendSpawnSummery(cardSpawn, client), 500);
+  setTimeout(async () => await sendSpawnSummary(cardSpawn, client), 500);
 };
 
-const sendSpawnSummery = async (cardSpawn: CardSpawn, client: Client) => {
-  const channel = await client.channels.fetch(cardSpawn.channelId);
-  if (channel === null) return;
-  if (!channel.isTextBased) return;
-  const textChannel = channel as TextChannel;
+const sendSpawnSummary = async (cardSpawn: CardSpawn, client: Client) => {
+  try {
+    const channel = await client.channels.fetch(cardSpawn.channelId);
+    if (channel === null) return;
+    if (!channel.isTextBased) return;
+    const textChannel = channel as TextChannel;
 
-  const permissions = textChannel.permissionsFor(client.user!);
-  const requiredPermissions = [
-    PermissionsBitField.Flags.SendMessages,
-    PermissionsBitField.Flags.ViewChannel,
-  ];
+    const permissions = textChannel.permissionsFor(client.user!);
+    console.log(permissions);
+    const requiredPermissions = [
+      PermissionsBitField.Flags.SendMessages,
+      PermissionsBitField.Flags.ViewChannel,
+    ];
 
-  if (!permissions?.has(requiredPermissions)) {
-    console.log(
-      `Bot lacks permission to view or send messages in the channel. - ${permissions}`,
+    if (!permissions?.has(requiredPermissions)) {
+      console.log(
+        `Bot lacks permission to view or send messages in the channel. - ${permissions}`,
+      );
+      return;
+    }
+
+    console.log(`Bot has permissions ${requiredPermissions}`);
+
+    const messages = await textChannel.messages.fetch({ limit: 100 });
+
+    const claim = cardSpawn.claims[0];
+    const expectedTitle =
+      claim.claimType === 'summon' ? 'Manual Summon' : 'Automatic Summon';
+    const spawnTime = new Date(claim.dateTime);
+    const despawnTime = new Date(spawnTime.getTime() + 20 * 1000);
+    const discordTimestamp = `<t:${Math.floor(despawnTime.getTime() / 1000)}:R>`;
+
+    const targetMessage = messages.find(
+      (msg) =>
+        msg.author.id === '1242388858897956906' &&
+        msg.embeds.some(
+          (embed) => embed.title && embed.title.includes(expectedTitle),
+        ),
     );
-    return;
+
+    console.log(targetMessage);
+
+    if (!targetMessage) return;
+
+    const claimContent = cardSpawn.claims.map((value, index): string => {
+      return `${index}: - **${value.card.name}** *${value.card.series}*\n`;
+    });
+
+    const content = `Card will despawn ${discordTimestamp}\n${claimContent}`;
+
+    const message = await targetMessage.reply(content);
+
+    setTimeout(async () => await message.delete(), 20000);
+  } catch (e) {
+    console.error(e);
   }
-
-  const messages = await textChannel.messages.fetch({ limit: 100 });
-
-  const claim = cardSpawn.claims[0];
-  const expectedTitle =
-    claim.claimType === 'summon' ? 'Manual Summon' : 'Automatic Summon';
-  const spawnTime = new Date(claim.dateTime);
-  const despawnTime = new Date(spawnTime.getTime() + 20 * 1000);
-  const discordTimestamp = `<t:${Math.floor(despawnTime.getTime() / 1000)}:R>`;
-
-  const targetMessage = messages.find(
-    (msg) =>
-      msg.author.id === '1242388858897956906' &&
-      msg.embeds.some(
-        (embed) => embed.title && embed.title.includes(expectedTitle),
-      ),
-  );
-
-  if (!targetMessage) return;
-
-  const claimContent = cardSpawn.claims.map((value, index): string => {
-    return `${index}: - **${value.card.name}** *${value.card.series}*\n`;
-  });
-
-  const content = `Card will despawn ${discordTimestamp}\n${claimContent}`;
-
-  const message = await targetMessage.reply(content);
-
-  setTimeout(async () => await message.delete(), 20000);
 };
