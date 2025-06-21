6eb8894c108c33c989beba2b2da8d9d2e352edc5
Peaches_MLG <PeachesMLG100@gmail.com>
2025-06-21 14:33:35 +0100

    default to false

--- src/handlers/cardSpawnHandler.ts
+++ src/handlers/cardSpawnHandler.ts
@@ -31,7 +31,7 @@ const createSummonTimer = async (cardSpawn: CardSpawn, client: Client) => {
     (await getSetting(
       cardSpawn.SummonedBy,
       SettingsTypes.AUTOMATIC_SUMMON_TIMERS,
-    )) ?? 'true';
+    )) ?? 'false';
 
   if (enabled !== 'true') return;
 
