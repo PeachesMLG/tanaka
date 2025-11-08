ef7035ad4d866374c99f4abf6ec19574251ce8f0
Peaches_MLG <PeachesMLG100@gmail.com>
2025-11-08 23:19:36 +0000

    Changed logs

--- src/handleConnection.ts
+++ src/handleConnection.ts
@@ -1,14 +1,6 @@
 import { Client } from 'discord.js';
 
 export const handleConnection = (client: Client) => {
-  client.on('reconnecting', (message) => {
-    console.log(`User Reconnecting`);
-  });
-
-  client.on('resume', (message) => {
-    console.log(`Connected ${client.user?.tag}`);
-  });
-
   client.on('disconnect', (message) => {
     console.log(`User Disconnected`);
     process.exit(1);
@@ -17,14 +9,6 @@ export const handleConnection = (client: Client) => {
   client.on('error', (err) => {
     console.error(err);
     process.exit(1);
-  });
-
-  client.on('shardReconnecting', (message) => {
-    console.log(`Shard Reconnecting`);
-  });
-
-  client.on('shardResume', (message) => {
-    console.log(`Shard Resumed`);
   });
 
   client.on('shardDisconnect', (message) => {
