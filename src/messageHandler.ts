17995ab693b6e117c5856a3fdd5c10218d900f49
Peaches_MLG <PeachesMLG100@gmail.com>
2025-12-10 17:36:53 +0000

    Oopsie

--- src/messageHandler.ts
+++ src/messageHandler.ts
@@ -37,19 +37,20 @@ const timers = [
   },
   {
     title: 'Date Reward Claimed!',
-    cooldown: 1800,
+    cooldown: 180,
+    premiumCooldown: 90,
     timerMessage: '</date:1410808024070619217>',
     timerInformation:
-      'Automatically triggered by Clan Summon\n Turn this off in the /user settings command',
+      'Automatically triggered by Dates\n Turn this off in the /user settings command',
     setting: SettingsTypes.AUTOMATIC_DATE_TIMERS,
   },
   {
     title: 'Clan War - Spell Summon!',
-    cooldown: 180,
+    cooldown: 1800,
     timerMessage: '</clan-summon:1448360325807214703>',
     timerInformation:
-      'Automatically triggered by Dates\n Turn this off in the /user settings command',
-    setting: SettingsTypes.AUTOMATIC_DATE_TIMERS,
+      'Automatically triggered by Clan Summon\n Turn this off in the /user settings command',
+    setting: SettingsTypes.AUTOMATIC_CLAN_SUMMONS,
   },
 ];
 
